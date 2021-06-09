f3ce749de885d7780313c6410b3fd8a37fb7f898
Explorer_Carbon <explorer_carbon@163.com>
2021-06-09 15:08:16 +0800

    教师端排课功能进一步实现

--- src/views/teacher/ModifyCourse.vue
+++ src/views/teacher/ModifyCourse.vue
@@ -1,59 +1,80 @@
 <template>
   <div class="div-main-content">
     <h1 class="font-title">教师排课页面</h1>
-    <h2 class="font-main-component-title">在这里添加课程</h2>
+    <h2 class="font-main-component-title">在这里新建课程</h2>
     <div class="div-normal-content">
-      <div class="font-normal-text">在下表填写需要新建的课程信息，填写完后按下“点击增加课程”按钮。</div>
+      <div class="font-normal-text">在下表填写需要新建的课程信息，填写完后按下“点击新建课程”按钮。</div>
       <table class="table-content">
         <thead>
         <tr>
-          <td>课程编号</td>
-          <td>课程名</td>
-          <td>上课周数</td>
-          <td>是否隔周上课</td>
-          <td>星期几上课</td>
-          <td>第几节课上课</td>
-          <td>容量（人数）</td>
+          <td>{{ htmlText.tableHeader.courseId }}</td>
+          <td>{{ htmlText.tableHeader.courseName }}</td>
+          <td>{{ htmlText.tableHeader.studentTotalNum }}</td>
         </tr>
         </thead>
         <tbody>
         <tr>
-          <td><input v-model="newCourseInfo.courseId" type="text" placeholder="请输入课程编号"/></td>
-          <td><input v-model="newCourseInfo.courseName" type="text" placeholder="请输入课程名"/></td>
-          <td>
-            第
-            <select v-model="newCourseInfo.weekStart">
-              <option v-for="num in weekNums" :value="num">{{ num }}</option>
-            </select>
-            周 —— 第
-            <select v-model="newCourseInfo.weekEnd">
-              <option v-for="num in weekNums" :value="num">{{ num }}</option>
-            </select>
-            周
-          </td>
-          <td><input v-model="newCourseInfo.isBiweekly" type="checkbox"/></td>
-          <td>
-            <select v-model="newCourseInfo.weekDay">
-              <option v-for="(weekDay,index) in weekDays" :value="index">{{ weekDay }}</option>
-            </select>
-          </td>
-          <td>
-            第
-            <select v-model="newCourseInfo.splitStart">
-              <option v-for="num in splitNums" :value="num">{{ num }}</option>
-            </select>
-            节 —— 第
-            <select v-model="newCourseInfo.splitEnd">
-              <option v-for="num in splitNums" :value="num">{{ num }}</option>
-            </select>
-            节
-          </td>
-          <td><input v-model="newCourseInfo.studentTotalNum" type="text" placeholder="请输入课程人数容量"/></td>
+          <td><input v-model="newCourseInfo.courseId" @blur="checkIsEmpty" type="text"
+                     placeholder="请输入课程编号"/></td>
+          <td><input v-model="newCourseInfo.courseName" @blur="checkIsEmpty" type="text"
+                     placeholder="请输入课程名"/></td>
+          <td><input v-model="newCourseInfo.studentTotalNum" @blur="checkIsEmpty"
+                     type="text" placeholder="请输入课程人数容量"/></td>
         </tr>
         </tbody>
       </table>
-      <br/>
-      <button @click="addCourse()">点击增加课程</button>
+      <table class="table-content">
+        <thead>
+        <tr>
+          <td>{{ htmlText.tableHeader.weekNums }}</td>
+          <td>{{ htmlText.tableHeader.isBiweekly }}</td>
+          <td>{{ htmlText.tableHeader.weekDay }}</td>
+          <td>{{ htmlText.tableHeader.splitNums }}</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>
+            第
+            <select v-model="newCourseInfo.weekStart" @change="checkIsEmpty">
+              <option v-for="num in selectOptions.weekStartNums" :value="num">{{ num }}</option>
+            </select>
+            周 —— 第
+            <select v-model="newCourseInfo.weekEnd" @change="checkIsEmpty">
+              <option v-for="num in selectOptions.weekEndNums" :value="num">{{ num }}</option>
+            </select>
+            周
+          </td>
+          <td>
+            <input v-model="newCourseInfo.isBiweekly" @change="checkIsEmpty" type="radio" name="isBiweekly"
+                   value="true"/>是
+            <input v-model="newCourseInfo.isBiweekly" @change="checkIsEmpty" type="radio" name="isBiweekly"
+                   value="false"/>否
+          </td>
+          <td>
+            <select v-model="newCourseInfo.weekDay" @change="checkIsEmpty">
+              <option v-for="(weekDay,index) in selectOptions.weekDays" :value="index+1">{{ weekDay }}</option>
+            </select>
+          </td>
+          <td>
+            第
+            <select v-model="newCourseInfo.splitStart" @change="checkIsEmpty">
+              <option v-for="num in selectOptions.splitStartNums" :value="num">{{ num }}</option>
+            </select>
+            节 —— 第
+            <select v-model="newCourseInfo.splitEnd" @change="checkIsEmpty">
+              <option v-for="num in selectOptions.splitEndNums" :value="num">{{ num }}</option>
+            </select>
+            节
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <div class="font-normal-text">
+        <span>提示：</span>
+        <span :class="htmlClass.newCourseTips">{{ htmlText.newCourseTips.value }}</span>
+      </div>
+      <button @click="addCourse()" id="btn-new-course" :disabled="htmlDisabled.btnNewCourse">点击新建课程</button>
     </div>
     <span class="span-main-component-line"/>
     <h2 class="font-main-component-title">您的现有课程</h2>
@@ -86,35 +107,67 @@
       </table>
     </div>
   </div>
+
 </template>
 
 <script>
-import {reactive} from "vue"
+import {reactive, ref} from "vue"
 import {request} from "@/assets/js/request"
 
 export default {
   name: "ModifyCourse",
   setup() {
-    let weekNums = [];
-    let splitNums = [];
-    let weekDays = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
+    let selectOptions = {
+      weekStartNums: [],
+      weekEndNums: [],
+      splitStartNums: [],
+      splitEndNums: [],
+      weekDays: ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    }
     let courses = reactive([{id: null, name: null, teacherName: null, weekNum: null, time: null, studentNum: null}]);
     let newCourseInfo = reactive({
       userId: null,
       courseId: null,
       courseName: null,
+      studentTotalNum: null,
       weekStart: null,
       weekEnd: null,
-      isBiweekly: false,
+      isBiweekly: null,
       weekDay: null,
       splitStart: null,
-      splitEnd: null,
-      studentTotalNum: null
+      splitEnd: null
     });
+    let courseInfoRange = {
+      minWeek: 1,
+      maxWeek: 21,
+      minSplitNum: 1,
+      maxSplitNum: 11
+    }
+    let htmlClass = reactive({
+      newCourseTips: "div-normal-content",
+    });
+    let htmlText = {
+      tableHeader: {
+        courseId: "课程编号",
+        courseName: "课程名",
+        studentTotalNum: "容量（人数）",
+        weekNums: "上课周数",
+        weekStart: "上课周数（起始值）",
+        weekEnd: "上课周数（结束值）",
+        isBiweekly: "是否隔周上课",
+        weekDay: "星期几上课",
+        splitNums: "第几节课上课",
+        splitStart: "第几节课上课（起始值）",
+        splitEnd: "第几节课上课（结束值）"
+      },
+      newCourseTips: ref("需要把表格信息填完才能点击新建课程"),
+    };
+    let htmlDisabled = reactive({
+      btnNewCourse: true,
+    })
 
     // TODO: 新增课程条件判断
     let addCourse = () => {
-      newCourseInfo.userId = window.sessionStorage.getItem("userId");
       request('/course/addCourse', newCourseInfo).then((response) => {
         alert(response.data.message);
       })
@@ -127,14 +180,64 @@ export default {
       })
     }
 
-    let init = () => {
-      for (let i = 1; i <= 21; i++) {
-        weekNums.push(i);
+    let checkIsEmpty = () => {
+      console.log("checkIsEmpty");
+      console.log(newCourseInfo);
+      let validity = true;
+      if (!newCourseInfo.userId) {
+        htmlText.newCourseTips.value = "您的登录信息无效，需要重新登录";
+        validity = false;
       }
-      for (let i = 1; i <= 11; i++) {
-        splitNums.push(i);
+      if (validity) {
+        for (let item in newCourseInfo) {
+          if (newCourseInfo.hasOwnProperty(item)) {
+            if (newCourseInfo[item] == null || newCourseInfo[item] === "") {
+              htmlText.newCourseTips.value = "表格数据尚未填写完整，您还需要填写”" + htmlText.tableHeader[item] + "“";
+              validity = false;
+              break;
+            }
+          }
+        }
+      }
+      if (validity) {
+        validity = checkNumRange();
+      }
+      if (validity) {
+        htmlText.newCourseTips.value = "表格数据已经填写完整，您可以点击新建课程了";
+        htmlClass.newCourseTips = "font-pass-text";
+        htmlDisabled.btnNewCourse = false;
+      } else {
+        htmlClass.newCourseTips = "font-warning-text";
+        htmlDisabled.btnNewCourse = true;
+      }
+    }
+
+    let checkNumRange = () => {
+      let validity = true;
+      if (newCourseInfo.weekStart > newCourseInfo.weekEnd) {
+        htmlText.newCourseTips.value = "数据填写有误：";
+        htmlText.newCourseTips.value += htmlText.tableHeader.weekStart + "不能大于" + htmlText.tableHeader.weekEnd;
+        validity = false;
+      } else if (newCourseInfo.splitStart > newCourseInfo.splitEnd) {
+        htmlText.newCourseTips.value = "数据填写有误：";
+        htmlText.newCourseTips.value += htmlText.tableHeader.splitStart + "不能大于" + htmlText.tableHeader.splitStart;
+        validity = false;
       }
 
+      return validity;
+    }
+
+    let init = () => {
+      for (let i = courseInfoRange.minWeek; i <= courseInfoRange.maxWeek; i++) {
+        selectOptions.weekStartNums.push(i);
+        selectOptions.weekEndNums.push(i);
+      }
+      for (let i = courseInfoRange.minSplitNum; i <= courseInfoRange.maxSplitNum; i++) {
+        selectOptions.splitStartNums.push(i);
+        selectOptions.splitEndNums.push(i);
+      }
+
+      newCourseInfo.userId = window.sessionStorage.getItem("userId");
       let requestData = {userId: window.sessionStorage.getItem("userId")};
       request('course/findAllCourseByTeacherId', requestData).then((response) => {
         courses.splice(0);
@@ -147,18 +250,22 @@ export default {
     init();
 
     return {
-      weekNums,
-      weekDays,
-      splitNums,
+      selectOptions,
       courses,
       newCourseInfo,
+      htmlClass,
+      htmlText,
+      htmlDisabled,
       addCourse,
-      deleteCourse
+      deleteCourse,
+      checkIsEmpty
     }
   }
 }
 </script>
 
 <style scoped>
-
+#btn-new-course {
+  margin: 10px 0;
+}
 </style>
